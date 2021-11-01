a4b5ed636b0f5b9c931b9855950adb825c439cc0
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-01 18:44:50 +0530

    Added the while loop to take input form console and add the other

--- src/com/birdgelabz/addressbook/AddressBook.java
+++ src/com/birdgelabz/addressbook/AddressBook.java
@@ -5,55 +5,92 @@ import java.util.Scanner;
 
 public class AddressBook {
 	// taking arraylist with class object and it will store the value dynamic
-    private static ArrayList<PersonDetail> list = new ArrayList<PersonDetail>();
-    private static Scanner scanner = new Scanner(System.in);
-    public static void main(String args[]){
-        //creating the persondetail class object to add the person detail
-        PersonDetail personDetail = addPerson();
-        // person detail object give to the list
-        list.add(personDetail);
-        for (int i=0;i<list.size();i++){
-           AddressBook.display(list.get(i)); //display method take the input as list and print the data
-        }
+	private static ArrayList<PersonDetail> list = new ArrayList<PersonDetail>();
+	private static Scanner scanner = new Scanner(System.in);
 
-    }
-    /* addperson method is to take input from the console
-    personDetail given as return type to the method as there are different type of parameter need to return the method
-     */
-    private static PersonDetail addPerson(){
-        System.out.println("Enter the firstname");
-        String firstName= scanner.next();
-        System.out.println("Enter the lastname");
-        String lastName= scanner.next();
-        System.out.println("Enter the address");
-        String address= scanner.next();
-        System.out.println("Enter the city");
-        String city= scanner.next();
-        System.out.println("Enter the state");
-        String state= scanner.next();
-        System.out.println("Enter the email");
-        String email= scanner.next();
-        System.out.println("Enter the zip");
-        long zip= scanner.nextLong();
-        System.out.println("Enter the phone of the person");
-        long phone= scanner.nextLong();
-        PersonDetail personDetail= new PersonDetail(firstName,lastName,address,city,state,email,zip,phone);
-        return personDetail;
-    }
-    /* Dispaly method is give to display the persom data
-    personDetail class obejct give as the parametr to the Display method
-     */
-    private static void display(PersonDetail personDetail){
-        System.out.println("firstname"+ personDetail.getFirstName());
-        System.out.println("lastname"+personDetail.getLastName());
-        System.out.println("address"+personDetail.getAddress());
-        System.out.println("city"+personDetail.getCity());
-        System.out.println("state"+personDetail.getState());
-        System.out.println("email"+ personDetail.getEmail());
-        System.out.println("zip"+ personDetail.getZip());
-        System.out.println("phone"+personDetail.getPhone());
-    }
+	public static void main(String args[]) {
+		// creating the persondetail class object to add the person detail
+		PersonDetail personDetail = addPerson();
+		// person detail object give to the list
+		boolean isAdd = true;
+		while (isAdd) {
+			PersonDetail personDetail1 = AddressBook.addPerson();
+			list.add(personDetail1);
+			System.out.println("Want to add other person");
+			System.out.println("Enter 1 or 0");
+			int num = scanner.nextInt();
+			if (num == 1)
+				isAdd = true;
+			else
+				isAdd = false;
+		}
+		list.add(personDetail);
+		for (int i = 0; i < list.size(); i++) {
+			AddressBook.display(list.get(i)); // display method take the input as list and print the data
+		}
+
+	}
+
+	/*
+	 * addperson method is to take input from the console personDetail given as
+	 * return type to the method as there are different type of parameter need to
+	 * return the method
+	 */
+	private static PersonDetail addPerson() {
+		System.out.println("Enter the firstname");
+		String firstName = scanner.next();
+		System.out.println("Enter the lastname");
+		String lastName = scanner.next();
+		System.out.println("Enter the address");
+		String address = scanner.next();
+		System.out.println("Enter the city");
+		String city = scanner.next();
+		System.out.println("Enter the state");
+		String state = scanner.next();
+		System.out.println("Enter the email");
+		String email = scanner.next();
+		System.out.println("Enter the zip");
+		long zip = scanner.nextLong();
+		System.out.println("Enter the phone of the person");
+		long phone = scanner.nextLong();
+		PersonDetail personDetail = new PersonDetail(firstName, lastName, address, city, state, email, zip, phone);
+		return personDetail;
+	}
+
+	private static PersonDetail AddPerson() {
+		System.out.println("Enter the firstname");
+		String firstName = scanner.next();
+		System.out.println("Enter the lastname");
+		String lastName = scanner.next();
+		System.out.println("Enter the address");
+		String address = scanner.next();
+		System.out.println("Enter the city");
+		String city = scanner.next();
+		System.out.println("Enter the state");
+		String state = scanner.next();
+		System.out.println("Enter the email");
+		String email = scanner.next();
+		System.out.println("Enter the zip");
+		long zip = scanner.nextLong();
+		System.out.println("Enter the phone of the person");
+		long phone = scanner.nextLong();
+		PersonDetail personDetail = new PersonDetail(firstName, lastName, address, city, state, email, zip, phone);
+		return personDetail;
+	}
+
+	/*
+	 * Dispaly method is give to display the persom data personDetail class obejct
+	 * give as the parametr to the Display method
+	 */
+	private static void display(PersonDetail personDetail) {
+		System.out.println("firstname" + personDetail.getFirstName());
+		System.out.println("lastname" + personDetail.getLastName());
+		System.out.println("address" + personDetail.getAddress());
+		System.out.println("city" + personDetail.getCity());
+		System.out.println("state" + personDetail.getState());
+		System.out.println("email" + personDetail.getEmail());
+		System.out.println("zip" + personDetail.getZip());
+		System.out.println("phone" + personDetail.getPhone());
+	}
 
 }
-
-
